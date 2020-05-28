8b519c6d2fba08e150f87dd9ad0a64af4dad1c85
Amy Franz <amy.franz@gmail.com>
2020-05-28 21:49:55 +0100

    routing tutorial mosh

---
+++ src/components/posts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import queryString from "query-string";
+
+const Posts = ({ match, location }) => {
+  const { year, month } = queryString.parse(location.search);
+  return (
+    <div>
+      <h1>Posts</h1>
+      Year: {year}, Month: {month}
+    </div>
+  );
+};
+
+export default Posts;
